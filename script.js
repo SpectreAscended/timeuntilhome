33d0224d38aedf879e22becca2a30a20d38fb39a
Cory Pollard <astandard87@gmail.com>
2022-08-07 00:13:07 -0600

    Added styling, and added a days calculation

--- script.js
+++ script.js
@@ -1,27 +1,19 @@
-const dateDisplay = document.querySelector('.date');
+const dayDisplay = document.getElementById('days');
+const minuteDisplay = document.getElementById('minutes');
 
 const today = new Date();
 
-const date = today.getDate();
-
-const day = today.getDay();
-
-const fullYear = today.getFullYear();
-
-const hours = today.getHours();
-
-const isoString = today.toISOString();
-
-console.log(isoString);
-
 const returnHome = new Date('2022-10-01T14:42:17.833Z');
 
-const millisecondsToOctober = returnHome - today;
+const millisecondsTilReturn = returnHome - today;
 
-const minutes = millisecondsToOctober / 60000;
+const minutes = millisecondsTilReturn / 60000;
+
+const days = minutes / 1440;
 
 console.log(minutes);
 
 console.log(24 * 60);
 
-dateDisplay.textContent = Math.round(minutes);
+dayDisplay.textContent = `${Math.round(days)}`;
+minuteDisplay.textContent = `${Math.round(minutes)}`;
