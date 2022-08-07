3371c1fdc1ac732ecdf87cc22321ac543d8fd245
Cory Pollard <astandard87@gmail.com>
2022-08-07 00:38:00 -0600

    fixed new feature of updated time

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
 const dayDisplay = document.getElementById('days');
 const minuteDisplay = document.getElementById('minutes');
 
-const today = new Date();
-
-const returnHome = new Date('2022-10-01T14:42:17.833Z');
-
-const millisecondsTilReturn = returnHome - today;
-
-const minutes = millisecondsTilReturn / 60000;
-
-const days = minutes / 1440;
-
 const displayTime = () => {
+  const today = new Date();
+  const returnHome = new Date('2022-10-01T08:00:00.833Z');
+  const millisecondsTilReturn = returnHome - today;
+  const minutes = millisecondsTilReturn / 60000;
+
+  const days = minutes / 1440;
   dayDisplay.textContent = `${Math.round(days)}`;
   minuteDisplay.textContent = `${Math.round(minutes)}`;
+  console.log('Time updated');
 };
 
 displayTime();
